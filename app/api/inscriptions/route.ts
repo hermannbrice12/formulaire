c1bb30a1540785820234fb2d578300ec670f7d7d
tchofo <hermannbrice12@gmail.com>
2026-02-24 14:16:28 +0100

    email

--- app/api/inscriptions/route.ts
+++ app/api/inscriptions/route.ts
@@ -69,7 +69,7 @@ export async function POST(request: NextRequest) {
 
     return NextResponse.json({
       success: true,
-       data[0],
+      data: data[0],  
     });
 
   } catch (err: any) {
